15b2b2e42a2c6121c47e0e52b0eb84f1434b7b1b
Johan Lindell <johan@lindell.me>
2017-03-14 18:57:49 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# vue-barcode
+# VueBarcode
+
+Add barcodes to your Vue application super easily with VueBarcode!
+
+It is a light wrapper for the [JsBarcode](https://github.com/lindell/JsBarcode) barcode library.
+
+## Install
+````
+npm install vue-barcode
+````
+
+## Use / Example
+Simple example with input binding. [Try it out with this JsFiddle!](http://jsfiddle.net/hfgan035/4/)
+````html
+<div id="app">
+  <input v-model="barcodeValue" /><br>
+  <barcode v-bind:value="barcodeValue">
+    Show this if the rendering fails.
+  </barcode>
+</div>
+````
+
+````javascript
+import VueBarcode from 'vue-barcode';
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    barcodeValue: 'test',
+  },
+  components: {
+    'barcode': VueBarcode
+  }
+})
+````
+
+# Options
+All options:
+
+`value`
+`format`
+`width`
+`height`
+`text`
+`font-options`
+`font`
+`text-align`
+`text-position`
+`text-margin`
+`font-size`
+`background`
+`lineColor`
+`margin`
+`margin-top`
+`margin-bottom`
+`margin-left`
+`margin-right`
+
+For more information, see the [JsBarcode documentation](https://github.com/lindell/JsBarcode/wiki/Options).
---
+++ index.js
@@ -0,0 +1,65 @@
+var JsBarcode = require('jsbarcode');
+
+var VueBarcode = {
+  template: `
+  <div>
+    <svg class="vue-barcode-element" v-show="valid"></svg>
+    <div v-show="!valid">
+      <slot></slot>
+    </div>
+   </div>`,
+  props: {
+    value: [String, Number],
+    format: [String],
+    width: [String, Number],
+    height: [String, Number],
+    text: [String, Number],
+    fontOptions : [String],
+    font: [String],
+    textAlign: [String],
+    textPosition: [String],
+    textMargin: [String, Number],
+    fontSize: [String, Number],
+    background: [String],
+    lineColor: [String],
+    margin: [String, Number],
+    marginTop: [String, Number],
+    marginBottom: [String, Number],
+    marginLeft: [String, Number],
+    marginRight: [String, Number],
+  },
+  mounted: function(){
+    this.$watch('$props', render, { deep: true, immediate: true });
+    render.call(this);
+  },
+  data: function(){
+    return {valid: true};
+  }
+};
+
+function render(){
+  JsBarcode(this.$el.querySelector('.vue-barcode-element'), this.value, {
+    format: this.format,
+    height: this.height,
+    width: this.width,
+    text: this.text,
+    fontOptions: this.fontOptions,
+    font: this.font,
+    textAlign: this.textAlign,
+    textPosition: this.textPosition,
+    textMargin: this.textMargin,
+    fontSize: this.fontSize,
+    background: this.background,
+    lineColor: this.lineColor,
+    margin: this.margin,
+    marginTop: this.marginTop,
+    marginBottom: this.marginBottom,
+    marginLeft: this.marginLeft,
+    marginRight: this.marginRight,
+    valid: valid => {
+      this.valid = valid;
+    }
+  });
+}
+
+module.exports = VueBarcode;
