e32c17f9efabe8a70725d1c959562033928e7e47
wachirametp <wachiramet.pai@gmail.com>
2017-06-15 14:57:31 +0700

    set default value

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var JsBarcode = require('jsbarcode');
+import JsBarcode from 'jsbarcode'
 
 var VueBarcode = {
   template: `
@@ -9,35 +9,35 @@ var VueBarcode = {
     </div>
    </div>`,
   props: {
-    value: [String, Number],
-    format: [String],
-    width: [String, Number],
-    height: [String, Number],
-    text: [String, Number],
-    fontOptions : [String],
-    font: [String],
-    textAlign: [String],
-    textPosition: [String],
-    textMargin: [String, Number],
-    fontSize: [String, Number],
-    background: [String],
-    lineColor: [String],
-    margin: [String, Number],
-    marginTop: [String, Number],
-    marginBottom: [String, Number],
-    marginLeft: [String, Number],
-    marginRight: [String, Number],
+    value: {type: [String, Number], default: undefined},
+    format: {type: [String], default: 'CODE128'},
+    width: {type: [String, Number], default: 2},
+    height: {type: [String, Number], default: 40},
+    text: {type: [String, Number], default: ''},
+    fontOptions: {type: [String], default: ''},
+    font: {type: [String], default: ''},
+    textAlign: {type: [String], default: 'center'},
+    textPosition: {type: [String], default: 'bottom'},
+    textMargin: {type: [String, Number], default: 0},
+    fontSize: {type: [String, Number], default: 12},
+    background: {type: [String], default: '#FFF'},
+    lineColor: {type: [String], default: '#000'},
+    margin: {type: [String, Number], default: 0},
+    marginTop: {type: [String, Number], default: 0},
+    marginBottom: {type: [String, Number], default: 0},
+    marginLeft: {type: [String, Number], default: 0},
+    marginRight: {type: [String, Number], default: 0}
   },
-  mounted: function(){
-    this.$watch('$props', render, { deep: true, immediate: true });
-    render.call(this);
+  mounted: function () {
+    this.$watch('$props', render, { deep: true, immediate: true })
+    render.call(this)
   },
-  data: function(){
-    return {valid: true};
+  data: function () {
+    return {valid: true}
   }
-};
+}
 
-function render(){
+function render () {
   JsBarcode(this.$el.querySelector('.vue-barcode-element'), this.value, {
     format: this.format,
     height: this.height,
@@ -57,9 +57,9 @@ function render(){
     marginLeft: this.marginLeft,
     marginRight: this.marginRight,
     valid: valid => {
-      this.valid = valid;
+      this.valid = valid
     }
-  });
+  })
 }
 
-module.exports = VueBarcode;
+export default VueBarcode
